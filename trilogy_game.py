7b247e7ed3a58f99fb82695d8cdd6e7084a8dc6a
ADARSH KUMAR SHAW <adarshkrshaw557@gmail.com>
2026-02-18 09:41:37 +0530

    Add files via upload

---
+++ trilogy_game.py
@@ -0,0 +1,44 @@
+import random 
+# this is a rock paper scissors game
+
+def get_choices( ):
+  player_choice = input ("Enter a choice (rock, paper, scissors): ")
+  options = ["rock ", "paper", "scissors"]
+  computer_choice = random.choice(options)
+  choices = {"player" : player_choice, "computer": computer_choice}
+  return choices
+
+
+def check_win(player,computer):
+  print (f"you chose  {player} , computer chose {computer}" )
+  if player == computer:
+    return "it's a tie"
+  elif player == "rock":
+    if computer == "scissors":
+      return "rock smashes scissors,You win !"
+    else :
+      return "paper covers rock,You lose "
+  elif player == "paper":
+    if computer == "rock":
+      return "paper covers rock ,You win !"
+    else :
+      return "scissor cuts paper ,You lose "
+  elif player == "scissors":
+    if computer == "paper":
+      return "scissors cut paper ,You win !"
+    else :
+      return "rock smashes scissors ,You lose "
+    
+choices = get_choices( )
+result = check_win (choices["player"], choices["computer"])
+print (result)
+
+
+
+
+
+
+
+
+
+
